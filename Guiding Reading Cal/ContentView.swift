fc74b83999a97b141930784dd206ff34fac28cfa
Eric Zulaica <ezulaica4@gmail.com>
2026-03-17 22:31:54 -0500

    Initial Commit

---
+++ Guiding Reading Cal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Guiding Reading Cal
+//
+//  Created by Eric Zulaica on 3/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Guiding Reading Cal/Guiding_Reading_CalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guiding_Reading_CalApp.swift
+//  Guiding Reading Cal
+//
+//  Created by Eric Zulaica on 3/17/26.
+//
+
+import SwiftUI
+
+@main
+struct Guiding_Reading_CalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
